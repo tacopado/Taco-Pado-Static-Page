bed33258cb65f6774ca04d8e6befae09fb72577f
Taco Pado <tacostacopado@gmail.com>
2020-08-10 16:38:03 -0300

    changed Combos

--- js/asd.js
+++ js/asd.js
@@ -5,3 +5,13 @@
      });
   }); // end of document ready
 });
+
+function leer_mas(btn) {
+    if($(btn).text()== "Leer más"){
+      $(btn).text("Menos");
+      $(btn).closest(".card-content").find(".truncate").toggleClass('truncate truncate_non');
+    }else{
+      $(btn).text("Leer más");
+      $(btn).closest(".card-content").find(".truncate_non").toggleClass('truncate_non truncate');
+    }
+}
